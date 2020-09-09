b4075cb13b6cba16f852f99b71ef75464297dd07
Thadd Schnulle <tschnulle@zynga.com>
2020-09-09 12:55:29 -0700

    More setup

---
+++ alto-coding-challenge/src/Main.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode, useState } from 'react';
+
+export default class Main extends React.Component {
+    componentDidMount() {
+
+    }
+
+    render(): ReactNode {
+        return <div className={'Main'}></div>;
+    }
+}
